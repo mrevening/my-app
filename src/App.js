f0ecae8c3634e2a73700d8972233b04f04e42794
mrevening <dominicwieczo@gmail.com>
2020-06-05 19:05:29 +0200

    new branch with contact cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from "react";
+// App.js
+import React, { useState } from "react";
+import "./styles.css";
+
 const App = () => {
-  const message = "This is my first variable rendered in JSX!"
   return (
-    <div>
-      <h1>Hello React World</h1>
-      <h2>{message}</h2>
+    <>
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name="Jenny Han"
+        email="jenny.han@notreal.com"
+        age={25}
+      />
+
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name="Jason Long"
+        email="jason.long@notreal.com"
+        age={45}
+      />
+
+      <ContactCard
+        avatar="https://via.placeholder.com/150"
+        name="Peter Pan"
+        email="peter.pan@neverland.com"
+        age={100}
+      />
+    </>
+  );
+};
+
+const ContactCard = props => {
+  const [showAge, setShowAge] = useState(false);
+
+  return (
+    <div className="contact-card">
+      <img src={props.avatar} alt="profile" />
+      <div className="user-details">
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        <button onClick={() => setShowAge(!showAge)}>
+          Toggle Age 
+        </button>
+        {showAge && <p>Age: {props.age}</p> }
+      </div>
     </div>
   );
-}
+};
 
 export default App;
